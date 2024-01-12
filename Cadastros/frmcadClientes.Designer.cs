bce655080313329d0d20e891c81be744ba6bf764
diegowoolley <154614603+diegowoolley@users.noreply.github.com>
2024-01-12 14:46:33 -0300

    Add files via upload

---
+++ Cadastros/frmcadClientes.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Sistema_de_Vendas
+{
+    partial class frmcadClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblnome = new System.Windows.Forms.Label();
+            this.lblendereco = new System.Windows.Forms.Label();
+            this.lbldocumento = new System.Windows.Forms.Label();
+            this.lbltelefone = new System.Windows.Forms.Label();
+            this.lblcelular = new System.Windows.Forms.Label();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.txtendereco = new System.Windows.Forms.TextBox();
+            this.txttelefone = new System.Windows.Forms.TextBox();
+            this.txtdocumento = new System.Windows.Forms.TextBox();
+            this.txtcelular = new System.Windows.Forms.TextBox();
+            this.pbFoto = new System.Windows.Forms.PictureBox();
+            this.dgCliente = new System.Windows.Forms.DataGridView();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnfoto = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCliente)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblnome
+            // 
+            this.lblnome.AutoSize = true;
+            this.lblnome.Location = new System.Drawing.Point(12, 9);
+            this.lblnome.Name = "lblnome";
+            this.lblnome.Size = new System.Drawing.Size(106, 13);
+            this.lblnome.TabIndex = 0;
+            this.lblnome.Text = "Nome/Razão Social:";
+            // 
+            // lblendereco
+            // 
+            this.lblendereco.AutoSize = true;
+            this.lblendereco.Location = new System.Drawing.Point(12, 37);
+            this.lblendereco.Name = "lblendereco";
+            this.lblendereco.Size = new System.Drawing.Size(56, 13);
+            this.lblendereco.TabIndex = 1;
+            this.lblendereco.Text = "Endereço:";
+            // 
+            // lbldocumento
+            // 
+            this.lbldocumento.AutoSize = true;
+            this.lbldocumento.Location = new System.Drawing.Point(325, 9);
+            this.lbldocumento.Name = "lbldocumento";
+            this.lbldocumento.Size = new System.Drawing.Size(52, 13);
+            this.lbldocumento.TabIndex = 2;
+            this.lbldocumento.Text = "Cpf/Cnpj:";
+            // 
+            // lbltelefone
+            // 
+            this.lbltelefone.AutoSize = true;
+            this.lbltelefone.Location = new System.Drawing.Point(12, 63);
+            this.lbltelefone.Name = "lbltelefone";
+            this.lbltelefone.Size = new System.Drawing.Size(52, 13);
+            this.lbltelefone.TabIndex = 3;
+            this.lbltelefone.Text = "Telefone:";
+            // 
+            // lblcelular
+            // 
+            this.lblcelular.AutoSize = true;
+            this.lblcelular.Location = new System.Drawing.Point(325, 63);
+            this.lblcelular.Name = "lblcelular";
+            this.lblcelular.Size = new System.Drawing.Size(42, 13);
+            this.lblcelular.TabIndex = 4;
+            this.lblcelular.Text = "Celular:";
+            // 
+            // txtnome
+            // 
+            this.txtnome.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtnome.Location = new System.Drawing.Point(124, 6);
+            this.txtnome.MaxLength = 100;
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(195, 20);
+            this.txtnome.TabIndex = 1;
+            // 
+            // txtendereco
+            // 
+            this.txtendereco.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtendereco.Location = new System.Drawing.Point(124, 34);
+            this.txtendereco.MaxLength = 100;
+            this.txtendereco.Name = "txtendereco";
+            this.txtendereco.Size = new System.Drawing.Size(195, 20);
+            this.txtendereco.TabIndex = 3;
+            // 
+            // txttelefone
+            // 
+            this.txttelefone.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txttelefone.Location = new System.Drawing.Point(124, 60);
+            this.txttelefone.MaxLength = 100;
+            this.txttelefone.Name = "txttelefone";
+            this.txttelefone.Size = new System.Drawing.Size(95, 20);
+            this.txttelefone.TabIndex = 4;
+            // 
+            // txtdocumento
+            // 
+            this.txtdocumento.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtdocumento.Location = new System.Drawing.Point(383, 6);
+            this.txtdocumento.MaxLength = 100;
+            this.txtdocumento.Name = "txtdocumento";
+            this.txtdocumento.Size = new System.Drawing.Size(195, 20);
+            this.txtdocumento.TabIndex = 2;
+            // 
+            // txtcelular
+            // 
+            this.txtcelular.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtcelular.Location = new System.Drawing.Point(383, 60);
+            this.txtcelular.MaxLength = 100;
+            this.txtcelular.Name = "txtcelular";
+            this.txtcelular.Size = new System.Drawing.Size(95, 20);
+            this.txtcelular.TabIndex = 5;
+            // 
+            // pbFoto
+            // 
+            this.pbFoto.BackgroundImage = global::Sistema_de_Vendas.Properties.Resources.download;
+            this.pbFoto.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbFoto.Location = new System.Drawing.Point(675, 6);
+            this.pbFoto.Name = "pbFoto";
+            this.pbFoto.Size = new System.Drawing.Size(100, 82);
+            this.pbFoto.TabIndex = 6;
+            this.pbFoto.TabStop = false;
+            // 
+            // dgCliente
+            // 
+            this.dgCliente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgCliente.Location = new System.Drawing.Point(12, 101);
+            this.dgCliente.Name = "dgCliente";
+            this.dgCliente.Size = new System.Drawing.Size(763, 150);
+            this.dgCliente.TabIndex = 7;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(15, 257);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(75, 23);
+            this.btnNovo.TabIndex = 8;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(111, 257);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 9;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(209, 257);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 10;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(302, 257);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 11;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(392, 257);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 12;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnfoto
+            // 
+            this.btnfoto.Location = new System.Drawing.Point(594, 37);
+            this.btnfoto.Name = "btnfoto";
+            this.btnfoto.Size = new System.Drawing.Size(75, 23);
+            this.btnfoto.TabIndex = 13;
+            this.btnfoto.Text = "foto";
+            this.btnfoto.UseVisualStyleBackColor = true;
+            // 
+            // frmcadClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 294);
+            this.Controls.Add(this.btnfoto);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.dgCliente);
+            this.Controls.Add(this.pbFoto);
+            this.Controls.Add(this.txtcelular);
+            this.Controls.Add(this.txtdocumento);
+            this.Controls.Add(this.txttelefone);
+            this.Controls.Add(this.txtendereco);
+            this.Controls.Add(this.txtnome);
+            this.Controls.Add(this.lblcelular);
+            this.Controls.Add(this.lbltelefone);
+            this.Controls.Add(this.lbldocumento);
+            this.Controls.Add(this.lblendereco);
+            this.Controls.Add(this.lblnome);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmcadClientes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Clientes";
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCliente)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblnome;
+        private System.Windows.Forms.Label lblendereco;
+        private System.Windows.Forms.Label lbldocumento;
+        private System.Windows.Forms.Label lbltelefone;
+        private System.Windows.Forms.Label lblcelular;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.TextBox txtendereco;
+        private System.Windows.Forms.TextBox txttelefone;
+        private System.Windows.Forms.TextBox txtdocumento;
+        private System.Windows.Forms.TextBox txtcelular;
+        private System.Windows.Forms.PictureBox pbFoto;
+        private System.Windows.Forms.DataGridView dgCliente;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnfoto;
+    }
+}
---
+++ Cadastros/frmcadClientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_Vendas
+{
+    public partial class frmcadClientes : Form
+    {
+        public frmcadClientes()
+        {
+            InitializeComponent();
+        }
+    }
+}
