59be7411ce9df2bbf77ee6613506a777460b441f
diegowoolley <154614603+diegowoolley@users.noreply.github.com>
2024-01-12 07:51:19 -0300

    Add files via upload

---
+++ Cadastros/frmcadUsuarios.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Sistema_de_Vendas.Cadastros
+{
+    partial class frmcadUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.lblPermissoes = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.cbPermissoes = new System.Windows.Forms.ComboBox();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(12, 31);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 13);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Location = new System.Drawing.Point(12, 57);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(41, 13);
+            this.lblSenha.TabIndex = 1;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // lblPermissoes
+            // 
+            this.lblPermissoes.AutoSize = true;
+            this.lblPermissoes.Location = new System.Drawing.Point(12, 83);
+            this.lblPermissoes.Name = "lblPermissoes";
+            this.lblPermissoes.Size = new System.Drawing.Size(63, 13);
+            this.lblPermissoes.TabIndex = 2;
+            this.lblPermissoes.Text = "Permissões:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNome.Enabled = false;
+            this.txtNome.Location = new System.Drawing.Point(81, 28);
+            this.txtNome.MaxLength = 100;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Enabled = false;
+            this.txtSenha.Location = new System.Drawing.Point(81, 54);
+            this.txtSenha.MaxLength = 20;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtSenha.TabIndex = 2;
+            // 
+            // cbPermissoes
+            // 
+            this.cbPermissoes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPermissoes.Enabled = false;
+            this.cbPermissoes.FormattingEnabled = true;
+            this.cbPermissoes.Items.AddRange(new object[] {
+            "Administrador",
+            "Usuário",
+            "Gerente",
+            "Support"});
+            this.cbPermissoes.Location = new System.Drawing.Point(81, 80);
+            this.cbPermissoes.Name = "cbPermissoes";
+            this.cbPermissoes.Size = new System.Drawing.Size(100, 21);
+            this.cbPermissoes.TabIndex = 3;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(12, 135);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(75, 23);
+            this.btnNovo.TabIndex = 4;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Enabled = false;
+            this.btnAdicionar.Location = new System.Drawing.Point(93, 135);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 5;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Enabled = false;
+            this.btnCancelar.Location = new System.Drawing.Point(174, 135);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // frmcadUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 170);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.cbPermissoes);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblPermissoes);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblNome);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmcadUsuarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Usuários";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Label lblPermissoes;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.ComboBox cbPermissoes;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
---
+++ Cadastros/frmcadUsuarios.cs
@@ -0,0 +1,96 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_Vendas.Cadastros
+{
+    public partial class frmcadUsuarios : Form
+    {
+        conn con = new conn();
+        string sql;
+        MySqlCommand cmd;
+
+        public frmcadUsuarios()
+        {
+            InitializeComponent();
+        }
+       
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+         //habilitar botões e campos
+            txtNome.Enabled = true;
+            txtSenha.Enabled = true;
+            cbPermissoes.Enabled = true;
+            btnAdicionar.Enabled = true;
+            btnCancelar.Enabled = true;
+            btnNovo.Enabled = false;
+            cbPermissoes.Text = "Administrador";
+            txtNome.Focus();
+        }
+        //verificar se campos estão vazios
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text.ToString().Trim() == "")
+            {
+                MessageBox.Show("Digite um nome para o usuário!");
+                txtNome.Clear();
+                txtNome.Focus();
+                return;
+            }
+            if (txtSenha.Text.ToString().Trim() == "")
+            {
+                MessageBox.Show("Digite uma senha para o usuário!");
+                txtSenha.Clear();
+                txtSenha.Focus();
+                return;
+                
+            }
+            else
+            {
+                try
+                {
+                    con.AbrirConexao();
+                    //inserir dados na tabela
+                    sql = "INSERT INTO cad_usuarios(nome_usuarios, senha_Usuarios, permissoes_Usuarios) VALUES (@nome_usuarios, @senha_Usuarios, @permissoes_Usuarios)";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.Parameters.AddWithValue("@nome_usuarios", txtNome.Text);
+                    cmd.Parameters.AddWithValue("@senha_Usuarios", txtSenha.Text);
+                    cmd.Parameters.AddWithValue("@permissoes_Usuarios", cbPermissoes.Text);
+                    cmd.ExecuteNonQuery();
+                    con.FecharConexao();
+                    //desabilitar botões e campos
+                    txtNome.Enabled = false;
+                    txtNome.Clear();
+                    txtSenha.Enabled = false;
+                    txtSenha.Clear();
+                    cbPermissoes.Enabled = false;
+                    btnAdicionar.Enabled = false;
+                    btnCancelar.Enabled = false;
+                    btnNovo.Enabled = true;
+                    MessageBox.Show("Usuário cadastado com sucesso!");
+                }
+                catch (Exception ex )
+                {
+
+                    MessageBox.Show("Erro ao cadastrar!" + ex);
+                }
+                
+            }
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+       
+    }
+}
